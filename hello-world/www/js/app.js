3adf998a68be16200b2a6874547c82d9ea9f507e
Garry Dolley <gdolley@arpnetworks.com>
2014-05-28 01:29:09 -0700

    Added ionicModal to controller

--- hello-world/www/js/app.js
+++ hello-world/www/js/app.js
@@ -1,10 +1,37 @@
 angular.module('todo', ['ionic'])
 
-.controller('TodoCtrl', function($scope) {
+.controller('TodoCtrl', function($scope, $ionicModal) {
   $scope.tasks = [
     { title: 'Learn more Ionic' },
     { title: 'Support tickets' },
     { title: 'Read books' },
     { title: 'Sleep' }
   ];
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide();
+    task.title = "";
+  };
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
 });
